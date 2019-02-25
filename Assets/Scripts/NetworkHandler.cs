a82379b099401c40868871a28b1a5b41ce86ba9f
Tiago Suzukayama <tiago.suzukayama@live.com>
2019-02-24 21:21:48 -0300

    added: mirror lib

---
+++ Assets/Scripts/NetworkHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class NetworkHandler : NetworkBehaviour
+{
+    public Camera camera;
+
+    void Start()
+    {
+
+        if (isLocalPlayer)
+        {
+            GameObject.Find("LobbyCamera").SetActive(false);
+            return;
+        }
+        // DISABLE CAMERA AND CONTROLS HERE (BECAUSE THEY ARE NOT ME)
+        camera.enabled = false;
+        //GetComponent<PlayerControls>().enabled = false;
+        //GetComponent<PlayerMovement>().enabled = false;
+    }
+}
