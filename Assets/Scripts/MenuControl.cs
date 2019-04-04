7edf1cda6d9b88bfd54d56a99dd01039eccb1cb8
Gustavo Murayama <gmurayama@outlook.com>
2019-04-04 18:09:00 -0300

    added: menu principal funcional

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MenuControl : MonoBehaviour
+{
+    public UnityEngine.UI.Text hostNameInput;
+
+    public void StartLocalGame()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinLocalGame()
+    {
+        if (hostNameInput.text != "Hostname")
+        {
+            NetworkManager.singleton.networkAddress = hostNameInput.text;
+        }
+        NetworkManager.singleton.StartClient();
+    }
+
+    void Start()
+    {
+        hostNameInput.text = NetworkManager.singleton.networkAddress;
+    }
+}
