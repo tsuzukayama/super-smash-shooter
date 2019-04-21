ca4225ca8c9d606588f5cbc15e2617cab8474a19
Tiago Suzukayama <tiago.suzukayama@live.com>
2019-04-20 22:02:36 -0300

    added: fixed gun and skin

---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] Transform Arm;
+
+    void Start()
+    {
+        transform.parent = Arm.transform;
+    }
+}
